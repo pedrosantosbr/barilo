fdf5aa0e9702d47d624c8b7cc8cf2dc1e99b8d54
Pedro Santos <pedro357bm@gmail.com>
2024-05-13 10:19:52 -0300

    chore: added filter on offer list

--- services/barilo-nextjs-chat-app/src/app/offers/page.tsx
+++ services/barilo-nextjs-chat-app/src/app/offers/page.tsx
@@ -1,8 +1,11 @@
+import { OffersList } from "@/components/offers/OffersList";
+
 export default function Offers() {
   return (
     <main className="flex min-h-screen overflow-y-auto flex-col items-center pt-10  pb-40">
       <div className="container space-y-10">
         <div className="text-center text-5xl font-bold">Ofertas do dia</div>
+        <OffersList />
       </div>
     </main>
   );
--- services/barilo-nextjs-chat-app/src/app/page.tsx
+++ services/barilo-nextjs-chat-app/src/app/page.tsx
@@ -25,9 +25,9 @@ export default function Home() {
                 Crie receitas incríveis e evite o desperdício de alimentos
                 utilizando apenas os ingredientes que você tem em casa.
               </p>
-              <Button variant={"link"} className="w-full">
-                Ver receitas <ArrowLongRightIcon className="ml-2 w-5" />
-              </Button>
+              <Link href={"/offers"} className="w-full flex items-center">
+                Ver encartes <ArrowLongRightIcon className="ml-2 w-5" />
+              </Link>
             </div>
           </div>
           <div className="lg:col-span-1">
@@ -45,9 +45,9 @@ export default function Home() {
                 Compare o preço dos produtos em diferentes supermercados e
                 escolha onde fazer a sua compra do mês.
               </p>
-              <Button variant={"link"} className="w-full">
-                Simular compra <ArrowLongRightIcon className="ml-2 w-5" />
-              </Button>
+              <Link href={"/offers"} className="w-full flex items-center">
+                Ver encartes <ArrowLongRightIcon className="ml-2 w-5" />
+              </Link>
             </div>
           </div>
           <div className="lg:col-span-1">
