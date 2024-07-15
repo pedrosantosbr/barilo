d0443315723c293489a1eb9c185e11a54ad90f47
Pedro Santos <pedro357bm@gmail>
2024-07-15 15:36:50 +0100

    chore: added blocking consumer instead of async io

--- lib/python/barilo/schemas/events.py
+++ lib/python/barilo/schemas/events.py
@@ -1,36 +1,13 @@
-from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, TypedDict
 
 
-@dataclass
-class Market:
-    id: str
-    name: str
+class ProductCreatedEvent(TypedDict):
+    class Market(TypedDict):
+        id: str
+        address: str
 
-@dataclass
-class Location:
-    id: str
-    address: str
-    cep: str
-
-@dataclass(frozen=True, slots=True)
-class Product:
-    id: str
+    market: Market
     name: str
     price: float
     weight: str
-    brand: Optional[str] = field(default=None)
-
-@dataclass(frozen=True, slots=True)
-class ProductEvent:
-    market: Market
-    location: Location
-    product: Product
-    
-    # def to_dict(self):
-    #     return {
-    #         "id": str(self.id),
-    #         "description": self.description,
-    #         "product_weight": self.product_weight,
-    #         "product_brand": self.product_brand,
-    #     }
+    brand: Optional[str]
