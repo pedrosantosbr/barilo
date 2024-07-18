ae9a1ed93c511e59821da369e2eb049037336dd0
Pedro Santos <pedro357bm@gmail>
2024-07-18 14:32:28 +0100

    chore: fetch products with prefetch

--- services/barilo-api/comparisons/views.py
+++ services/barilo-api/comparisons/views.py
@@ -5,6 +5,10 @@ from products.models import Product
 from rest_framework import serializers, generics
 from django.db.models import Prefetch
 
+import structlog
+
+logger = structlog.get_logger(__name__)
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,11 +25,39 @@ class ProductBucketItemSerializer(serializers.ModelSerializer):
 
 
 class ProductBucketSerializer(serializers.ModelSerializer):
-    product_bucket_items = ProductBucketItemSerializer(many=True, read_only=True)
+    # product_bucket_items = ProductBucketItemSerializer(many=True, read_only=True)
+
+    total = serializers.SerializerMethodField()
+    products = serializers.SerializerMethodField()
+    lower_price = serializers.SerializerMethodField()
+    max_price = serializers.SerializerMethodField()
 
     class Meta:
         model = ProductBucket
-        fields = ["id", "name", "product_bucket_items"]
+        fields = ["id", "name", "total", "products", "lower_price", "max_price"]
+
+    def get_total(self, obj):
+        return len(obj.product_bucket_items)
+
+    def get_products(self, obj):
+        products = []
+
+        for product_bucket_item in obj.product_bucket_items:
+            products.append(ProductSerializer(product_bucket_item.product).data)
+
+        return products
+
+    def get_lower_price(self, obj):
+        prices = []
+        for product in obj.product_bucket_items:
+            prices.append(product.product.price)
+        return min(prices)
+
+    def get_max_price(self, obj):
+        prices = []
+        for product in obj.product_bucket_items:
+            prices.append(product.product.price)
+        return max(prices)
 
 
 class ProductBucketSearchFilter(filters.SearchFilter):
@@ -36,7 +68,7 @@ class SearchProcuctBucketListView(generics.ListAPIView):
     queryset = ProductBucket.objects.prefetch_related(
         Prefetch(
             lookup="productbucketitem_set",
-            queryset=ProductBucketItem.objects.select_related("product").all(),
+            queryset=ProductBucketItem.objects.prefetch_related("product").all(),
             to_attr="product_bucket_items",
         )
     ).all()
